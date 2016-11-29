e15dcf1d8fbc8dcafc14a5f4e31331c3ff485e1d
leela <leela@leela-ross.com>
2016-11-29 12:40:30 +1100

    Changes to be committed: 	modified:   par2.sh Updatesas suggsted by reddit user whetu, kept check for files the same as the logic broke and it was trying to update empty folders

--- par2.sh
+++ par2.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
-# This script is created to go recursively into folders detect files and create Par2 files to protect data
+# This script is created to go recursively into folders,  
+# detect files and create Par2 files to protect data
 # Created by Leela Ross - leela@leela-ross.com 28/11/2016
 
 # Get running directory
-root=$(pwd)
+root=$PWD
 counter=1
 
 # Start loop based on tree output
-tree -dfi | while read line; do
-        pdir="$line"
-        dir=$(echo $pdir | sed -e "s/.//" -e "s/'/'/") # This is to remove the ./ from each line for when we tack the absolute path on
-        if [[ "$root$dir" == "$root" ]] && (($counter > 2)) # Detecting if the loop is completed by waiting for the blank line from tree
-        then
-                exit 1
-        fi
-        cd "$root$dir"
-        find=$(find . -mindepth 1 -maxdepth 1 -name "*.*") # seeing if there are any files in the directory
-        if [[ "$find" != "" ]]
-        then
-                if [ -e "$root$dir/.mfolder.par2" ] # Detecting to see if a par2 files already exists
-                then # if par2 exists then run a repair
-                        pwd
-                        par2repair -q -a .mfolder
-                else # if no par2 file create one
-                        pwd
-                        par2create -q -a .mfolder *
-                fi
-        fi
-        let counter=counter+1 # loop counter so the script doesn't stop on the first directory
+tree -dfi | while read -r line; do
+  pdir="$line"
+  # This is to remove the ./ from each line for when we tack the absolute path on
+  dir=$(sed -e "s/.//" -e "s/'/'/" <<< "$pdir") 
+
+  # Detecting if the loop is completed by waiting for the blank line from tree
+  if [[ "$root$dir" == "$root" ]] && ((counter > 2)); then
+    exit 1
+  fi
+
+  cd "$root$dir" || exit 1
+
+  # seeing if there are any files in the directory
+  find=$(find . -mindepth 1 -maxdepth 1 -name "*.*") 
+  if [[ "$find" != "" ]]; then
+    # Detecting to see if a par2 files already exists
+    # if par2 exists then run a repair
+    if [[ -e "$root$dir/.mfolder.par2" ]]; then
+      pwd
+      par2repair -q -a .mfolder
+    # if no par2 file create one
+    else 
+      pwd
+      par2create -q -a .mfolder ./*
+    fi
+  fi
+  # loop counter so the script doesn't stop on the first directory
+  let counter++ 
 done
