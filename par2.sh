3f94bea4647d66f4f93a9f15b7cb89db72ce1d90
leela <leela@leela-ross.com>
2016-11-30 20:48:04 +1100

    Updated varibles to use more descriptive names and decalred them at the begining of the script

--- par2.sh
+++ par2.sh
@@ -4,29 +4,36 @@
 # Created by Leela Ross - leela@leela-ross.com 28/11/2016
 # Thanks to Reddit Users whetu and 1or2. 
 
+# Declare Vars
+declare currentdir
+declare cleandir
+declare fsroot
+declare findresult
+declare counter
+
 # Get running directory
-root=$PWD
+fsroot=$PWD
 counter=1
 
 # Start loop based on tree output
 tree -dfi | while read -r line; do
-  pdir="$line"
+  currentdir="$line"
   # This is to remove the ./ from each line for when we tack the absolute path on
-  dir=$(sed -e "s/.//" -e "s/'/'/" <<< "$pdir") 
+  cleandir=$(sed -e "s/.//" -e "s/'/'/" <<< "$currentdir") 
 
   # Detecting if the loop is completed by waiting for the blank line from tree
-  if [[ "$root$dir" == "$root" ]] && ((counter > 2)); then
+  if [[ "$fsroot$cleandir" == "$fsroot" ]] && ((counter > 2)); then
     exit 1
   fi
 
-  cd "$root$dir" || exit 1
+  cd "$fsroot$cleandir" || exit 1
 
   # seeing if there are any files in the directory
-  find=$(find . -mindepth 1 -maxdepth 1 -name "*.*") 
-  if [[ "$find" != "" ]]; then
+  findresult=$(find . -mindepth 1 -maxdepth 1 -name "*.*") 
+  if [[ "$findresult" != "" ]]; then
     # Detecting to see if a par2 files already exists
     # if par2 exists then run a repair
-    if [[ -e "$root$dir/.mfolder.par2" ]]; then
+    if [[ -e "$fsroot$cleandir/.mfolder.par2" ]]; then
       pwd
       par2repair -q -a .mfolder
     # if no par2 file create one
